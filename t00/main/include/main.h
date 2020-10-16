a2da240737f6b3344ffd1a0bf569a1267ea5e3e7
Vitaliiy Syveniuk <vsyveniu@Vitaliiys-Mac-Pro.local>
2020-10-16 15:23:30 +0300

    initial

---
+++ t00/main/include/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include <string.h>
+#include <stdio.h>
+#include "freertos/semphr.h"
+#include "defines.h"
+#include "power_up.h"
+#include "oled.h"
+#include "photoresistor.h"
+
+#endif
---
+++ t00/main/main.c
@@ -0,0 +1,10 @@
+/** @file main.c *
+* entry point
+*/
+
+#include "main.h"
+
+void app_main()
+{
+
+}
