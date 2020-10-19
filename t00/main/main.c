6b6d49efdb655e18905c17ff4b74f11381c9f37c
Vitaliy Syvenuk <vsyvenuk@e1r1p6.unit.ua>
2020-10-19 12:07:13 +0300

    del build

--- t00/main/main.c
+++ t00/main/main.c
@@ -3,8 +3,21 @@
 */
 
 #include "main.h"
+#include "esp_wifi.h"
 
 void app_main()
 {
+    wifi_init_config_t wifi_config  = WIFI_INIT_CONFIG_DEFAULT();
 
+    esp_wifi_init(&wifi_config);
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+
+  /*   wifi_mode_t mode;
+
+    esp_wifi_get_mode(&mode);
+
+    printf("%d\n", mode); */
+
+    
 }
